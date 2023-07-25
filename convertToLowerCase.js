e9bacf2672ff1f3902600b82d1de33eb44ed6ca6
Sanjay krishnan <er.sanjaykrishnan@gmail.com>
2023-07-25 15:02:43 +0530

    Create convertToLowerCase.js

---
+++ convertToLowerCase.js
@@ -0,0 +1,7 @@
+function convertToLowerCase(string){
+    let input = prompt('enter a string in uppercase: ');
+    let output = input.toLowerCase();
+    return output;
+}
+
+console.log(convertToLowerCase());
