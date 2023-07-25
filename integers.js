7eee0700cac7b3ef09ff420272fd1fbaf3807e09
Sanjay krishnan <er.sanjaykrishnan@gmail.com>
2023-07-25 15:34:12 +0530

    Create integers.js

---
+++ integers.js
@@ -0,0 +1,8 @@
+function numberLine(){
+    let num = Number(prompt('enter a num: '));
+    if(num<0) console.log('negative');
+    else if (num>0) console.log('positive');
+    else  console.log('zero');
+    
+}
+numberLine();
